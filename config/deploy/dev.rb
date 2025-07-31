a4d9e65afadc87912b3204e3e2c3587d98ab4872
goldsmithb <bg2918@columbia.edu>
2025-07-31 15:26:08 -0400

    remove .bundle from linked dirs

--- config/deploy/dev.rb
+++ config/deploy/dev.rb
@@ -2,4 +2,4 @@
 
 set :stage, :dev
 set :branch, 'deployment'
-server 'diglib-service-prod1.cul.columbia.edu', user: fetch(:remote_user), roles: %w[app db]
+server 'diglib-service-prod1.cul.columbia.edu', user: fetch(:remote_user), roles: %w[app]
