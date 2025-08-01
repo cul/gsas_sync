dc8e2b2f4075ff1fd9dc287af1b433e060b36f31
goldsmithb <bg2918@columbia.edu>
2025-08-01 13:32:17 -0400

    typo

--- config/deploy.rb
+++ config/deploy.rb
@@ -22,7 +22,7 @@ set :whenever_roles, %w[app]
 set :whenever_environment, -> { fetch(:stage) }
 set :whenever_variables, -> { "'script_env=#{fetch(:deploy_name)}'" }
 set :whenever_command, lambda {
-  "whenever #--set 'script_env=#{fetch(:deploy_name)}&path=#{deploy_to}/current/gsas_sync_main.rb&rvm_prefix=#{fetch(:rvm_command_prefix)}'"
+  "whenever --set 'script_env=#{fetch(:deploy_name)}&path=#{deploy_to}/current/gsas_sync_main.rb&rvm_prefix=#{fetch(:rvm_command_prefix)}'"
 }
 
 # Configure location where capistrano.log will be written
