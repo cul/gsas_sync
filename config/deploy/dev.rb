9cfef7bbe8a5694d32ffb4d36f2e8f1e15eb09d2
goldsmithb <bg2918@columbia.edu>
2025-08-21 10:27:18 -0400

    remove capistrano role from dev server

--- config/deploy/dev.rb
+++ config/deploy/dev.rb
@@ -1,4 +1,4 @@
 # frozen_string_literal: true
 
 set :stage, :dev
-server 'diglib-service-prod1.cul.columbia.edu', user: fetch(:remote_user), roles: %w[production_app]
+server 'diglib-service-prod1.cul.columbia.edu', user: fetch(:remote_user)
