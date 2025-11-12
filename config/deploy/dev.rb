5c3c118fe99297f1fee34a9eb50ea36483b909fd
goldsmithb <bg2918@columbia.edu>
2025-11-12 15:56:44 -0500

    final deployment changes

--- config/deploy/dev.rb
+++ config/deploy/dev.rb
@@ -2,3 +2,5 @@
 
 set :stage, :dev
 server 'diglib-service-prod1.cul.columbia.edu', user: fetch(:remote_user)
+
+ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
--- config/deploy/prod.rb
+++ config/deploy/prod.rb
@@ -2,3 +2,6 @@
 
 set :stage, :prod
 server 'diglib-service-prod1.cul.columbia.edu', user: fetch(:remote_user), roles: %w[production_app]
+
+# Grab latest tag version
+ask :branch, proc { `git tag --sort=version:refname`.split("\n").last }
