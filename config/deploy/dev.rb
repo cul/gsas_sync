c66d561fc1f09d5a4a97080db6ae2e3653a585e5
goldsmithb <bg2918@columbia.edu>
2025-11-13 13:51:02 -0500

    dev

--- config/deploy/dev.rb
+++ config/deploy/dev.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
 set :stage, :dev
-server 'diglib-service-prod1.cul.columbia.edu', user: fetch(:remote_user)
+server 'diglib-service-prod1.cul.columbia.edu', user: fetch(:remote_user), roles: %w[production_app]
 
 ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
--- config/schedule.rb
+++ config/schedule.rb
@@ -12,18 +12,14 @@
 set :email_subject, 'GSAS Sync Cron Error (via Whenever Gem)'
 set :error_recipient, 'bg2918@columbia.edu' # Todo change to dev email the config yml file
 set :job_template, "/usr/local/bin/mailifrc -s 'Error - :email_subject' :error_recipient -- /bin/bash -l -c ':job'"
-puts 'whenever.rb file:'
-puts "@script_env#{@script_env}"
 
 # Override default rake task job type
 # job_type :rake, 'cd :path && :environment_variable=:environment bundle exec rake :task --silent :output'
 
 ############################  COMMAND  #########################################
 # Run dissertation sync script once a day on days 1-7 of each month:
-every '0 0 1-7 * *', at: '12:01 am' do
+every '0 0 1-7 * *' do
   command "echo 'script_env is : #{@script_env}' > /tmp/script_env.txt"
-  unless @script_env == 'gsas_sync_prod' # rubocop:disable Style/IfUnlessModifier
-    command "#{@rvm_command_prefix} #{@path}/gsas_sync_main.rb"
-  end
+  command "#{@rvm_command_prefix} #{@path}/gsas_sync_main.rb" if @script_env == 'gsas_sync_prod'
 end
 ################################################################################
